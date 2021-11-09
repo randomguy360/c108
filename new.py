8a7d50e75f5a78b2c8bf9df41924cb3268791131
randomguy360 <60027721+randomguy360@users.noreply.github.com>
2021-11-09 16:41:53 +0530

    Add files via upload

---
+++ new.py
@@ -0,0 +1,13 @@
+import plotly.express as px
+import random
+count = []
+dice_result = []
+
+for i in range(0,100):
+        dice1 = random.randint(1,6)
+        dice2 = random.randint(1,6)
+        dice_result.append(dice1 + dice2)
+        count.append(i)
+
+fig = px.bar(x = dice_result,y = count )
+fig.show()
